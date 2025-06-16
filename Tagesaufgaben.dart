c148aa36b529186ac327a0f30f9a08ac408e7d8b
Selihei <selina.heinrichlp@gmail.com>
2025-06-16 15:36:07 +0200

    added more structure menu

--- Tagesaufgaben.dart
+++ Tagesaufgaben.dart
@@ -1,7 +1,8 @@
 import 'dart:io';
 
-List<String> aufgaben= [];
+List<String> aufgaben= []; //noch einfügen
 int eingabe = 0;
+bool option = false;
 bool ausgang = false;
 
 void main (){
@@ -9,25 +10,77 @@ print("Welche Aktion soll ausgeführt werden? \n"
 "1 Übersicht \n"
 "2 Aufgaben hinzufügen \n"
 "3 Aufgaben löschen \n"
-"4 Aufgaben bearbeiten");
+"4 Aufgaben bearbeiten"); //Maybe zusammenfassen
 
 while(ausgang ==false)
 {
-    String input = stdin.readLineSync() ?? "0";
+    String? input = stdin.readLineSync();
+    if(input == "exit")
+    ausgang = true;
+    else if(input == "back"){
+    option = false;
+    main();}
+    else if(input == null)
+    input = "0";
+    else
+    
+    
     eingabe = int.parse(input); //Validation
 
+if(option == false) {
     switch(eingabe) {
     case 1:
+    option = true;
     option1();
     case 2:
-    print("Test2");
-    ausgang = true;
+    option = true;
+    option2();
+    case 3:
+    option = true;
+    option3();
+    case 4:
+    option = true;
+    option4();
     default:
     print("Ungültige Eingabe");
     }
 }
 }
+}
+
 void option1(){
-    print("Testttt");
+    if(ausgang == true)
+    return;
+
+    else{
+    print("Option 1");
+    }
+}
+
+void option2(){
+if(ausgang == true)
+    return;
+
+    else{
+    print("Option 2");
+    }
+}
+
+void option3(){
+if(ausgang == true)
+    return;
+
+    else{
+    print("Option 3");
+    }
+}
+
+void option4(){
+if(ausgang == true)
+    return;
+
+    else{
+    print("Option 4");
+    }
 
 }
