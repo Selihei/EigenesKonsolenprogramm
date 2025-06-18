502d6a7fba5a2074021859480edfc45f3c160d2b
Selihei <selina.heinrichlp@gmail.com>
2025-06-18 11:05:50 +0200

    more function

--- Tagesaufgaben.dart
+++ Tagesaufgaben.dart
@@ -1,11 +1,12 @@
 import 'dart:io';
 
 
-List<String> aufgaben= ["TaskSheet", "Vorlesung", "Tutorium"]; //noch einfügen
+List<String> aufgaben= ["Vorlesung", "TaskSheet", "Tutorium"]; //noch einfügen
 int eingabe = 0;
  //DateTime now = DateTime.now();
 bool option = false;
 bool ausgang = false;
+bool zuruck = false;
 
 void main (){
 print(
@@ -15,14 +16,16 @@ print(
 "1 Übersicht \n"
 "2 Aufgaben hinzufügen \n"
 "3 Aufgaben löschen \n"
-"4 Aufgaben bearbeiten"); //Maybe zusammenfassen
+"4 Aufgaben bearbeiten");
 
 while(ausgang ==false)
 {
+    zuruck = false;
     String? input = stdin.readLineSync();
     if(input == "exit")
     ausgang = true;
     else if(input == "back"){
+    zuruck = true;
     option = false;
     main();}
     else if(input == null)
@@ -33,7 +36,7 @@ while(ausgang ==false)
  
 
 if(option == false) {
-   eingabe = int.parse(input); //Validation
+   eingabe = int.parse(input); //Validation fehlt noch
 
     switch(eingabe) {
     case 1:
@@ -57,7 +60,7 @@ if(option == false) {
 }
 
 void option1(){
-    if(ausgang == true)
+    if(ausgang == true) //Blockt den Aufruf bei der Eingabe Exit
     return;
 
     else{
@@ -69,33 +72,58 @@ void option1(){
 }
 
 void option2(){
-if(ausgang == true)
+if(ausgang == true) //Blockt den Aufruf bei der Eingabe Exit
     return;
 
     else{
-    print("Option 2");
-    option1();
+        while(zuruck != true){
+    print("Aktuelle Liste:");
+     option1();
+    
+   print("Tippe die neue Aufgabe ein");
+
     String? input = stdin.readLineSync();
     if(input == null)
     print("Ungültige Eingabe");
+    else if(input == "back"){
+    zuruck = true;
+    option = false;
+    break;
+    }
+
     else
     aufgaben.add(input);
-    option1();
-   // print("$input");
+    }
     }
 }
 
 void option3(){
-if(ausgang == true)
+if(ausgang == true) //Blockt den Aufruf bei der Eingabe Exit
     return;
 
     else{
     print("Option 3");
+     for(int i = 0; i < aufgaben.length; i++){
+        print("$i ${aufgaben[i]}");
+    }
+    print("Welche Aufgabe soll gelöscht werden?");
+    String? input = stdin.readLineSync() ?? "0";
+   
+    if(input == null)
+    input = "0";
+ 
+ eingabe = int.parse(input); //Validation
+    
+    if( eingabe < 0 || eingabe > aufgaben.length)
+    print("Falsche Eingabe");
+    else{
+    aufgaben.removeAt(eingabe);
+    }
     }
 }
 
 void option4(){
-if(ausgang == true)
+if(ausgang == true) //Blockt den Aufruf bei der Eingabe Exit
     return;
 
     else{
